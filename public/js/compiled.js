a3ad1bf1930eee9c212ddb4693bef393e424916a
Erin Drummond <erin.dru@gmail.com>
2014-06-25 20:32:30 +1200

    aaand run grant task for previous commit. I really should stop forgetting to do this

--- public/js/compiled.js
+++ public/js/compiled.js
@@ -262,7 +262,7 @@ angular.module("pizza-app").controller("AdminController", function(BASE_URL, $sc
 
 	$scope.authenticate = function(password) {
 		$scope.error = "";
-		$http.post(BASE_URL + "orders/validate-password", { password : password }).then(function(data) {
+		$http.post(BASE_URL + "/orders/validate-password", { password : password }).then(function(data) {
 			console.log(data);
 			if (data.data.valid) {
 				$scope.authenticated = true;
@@ -274,14 +274,14 @@ angular.module("pizza-app").controller("AdminController", function(BASE_URL, $sc
 	}
 
 	$scope.loadOrders = function() {
-		$http.get(BASE_URL + "orders", { params: { password : $scope.password }}).then(function(data) {
+		$http.get(BASE_URL + "/orders", { params: { password : $scope.password }}).then(function(data) {
 			$scope.orders = data.data;
 		});
 	}
 
 	$scope.removeOrder = function(order) {
 		if (!confirm("Are you sure?")) { return; }
-		$http.delete(BASE_URL + "orders", { params: { id : order.id, password: $scope.password }}).then(function(data) {
+		$http.delete(BASE_URL + "/orders", { params: { id : order.id, password: $scope.password }}).then(function(data) {
 			$scope.loadOrders();
 		});
 	}
